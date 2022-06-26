e9a49b935be43942752a3cab65baa391be8c6cde
arsenalahm <clevercake7@gmail.com>
2022-06-26 17:41:36 +0800

    Add several uis and scale augmentation

---
+++ Assets/Script/Augmentation.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.XR;
+
+public class Augmentation : MonoBehaviour
+{
+    private float INIT_FRAMES = 300;
+
+    private float augFrames;
+    private float curFrames = 0;
+
+    private float timer = 0f;
+
+    public bool scaleAug = false;
+    private Vector3 oriScale;
+    private Vector3 tarScale;
+    private Vector3 minScale = new Vector3(0.05f, 0.05f, 0.05f);
+    private Vector3 maxScale = new Vector3(0.15f, 0.15f, 0.15f);
+
+    void SwapVector3(Vector3 x, Vector3 y)
+    {
+        Vector3 temp = x;
+        x = y;
+        y = temp;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        augFrames = INIT_FRAMES;
+        oriScale = this.transform.localScale;
+        tarScale = oriScale * 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var eyeTrackingData = TobiiXR.GetEyeTrackingData(TobiiXR_TrackingSpace.World);
+        if(eyeTrackingData.GazeRay.IsValid)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(eyeTrackingData.GazeRay.Origin,eyeTrackingData.GazeRay.Direction, out hit, Mathf.Infinity))
+            {
+                if (Equals(hit.transform, this.transform))
+                {
+                    timer += Time.deltaTime;
+                }
+                else
+                {
+                    timer = 0f;
+                }
+            }
+            if (timer >= 1.0f)
+            {
+                scaleAug = false;
+                augFrames = INIT_FRAMES;
+                curFrames = 0;
+            }
+        }
+        if (scaleAug)
+        {
+            float interpolationRatio = (float) curFrames / augFrames;
+            Vector3 interpolatedScale = Vector3.Lerp(oriScale, tarScale, interpolationRatio);
+            this.transform.localScale = interpolatedScale;
+            curFrames = (curFrames + 1) % (augFrames + 1);
+            if (curFrames == augFrames)
+            {
+                augFrames *= 0.8f;
+                SwapVector3(oriScale, tarScale);
+            }
+        }
+    }
+}
--- Assets/Script/GetGaze.cs
+++ Assets/Script/GetGaze.cs
@@ -23,12 +23,22 @@ public class GetGaze : MonoBehaviour
         {
             // The origin of the gaze ray is a 3D point
             var rayOrigin = eyeTrackingData.GazeRay.Origin;
-            print(rayOrigin);
 
             // The direction of the gaze ray is a normalized direction vector
             var rayDirection = eyeTrackingData.GazeRay.Direction;
 
             gazeCursor.transform.position = rayOrigin + 1.0f * rayDirection;
         }
+
+        // // For social use cases, data in local space may be easier to work with
+        // var eyeTrackingDataLocal = TobiiXR.GetEyeTrackingData(TobiiXR_TrackingSpace.Local);
+
+        // // The EyeBlinking bool is true when the eye is closed
+        // var isLeftEyeBlinking = eyeTrackingDataLocal.IsLeftEyeBlinking;
+        // var isRightEyeBlinking = eyeTrackingDataLocal.IsRightEyeBlinking;
+
+        // // Using gaze direction in local space makes it easier to apply a local rotation
+        // // to your virtual eye balls.
+        // var eyesDirection = eyeTrackingDataLocal.GazeRay.Direction;
     }
 }
