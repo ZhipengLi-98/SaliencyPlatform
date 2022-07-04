f9c2cfe545d2861c6eb4ce16ba8b9b19d7d6e1cd
arsenalahm <clevercake7@gmail.com>
2022-07-04 22:47:10 +0800

    Add data writer

--- Assets/Script/AugmentationController.cs
+++ Assets/Script/AugmentationController.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Tobii.XR;
+using System.IO;
 
 public class AugmentationController : MonoBehaviour
 {
     public List<GameObject> userInterefaces = new List<GameObject>();
+
+    public GameObject camera;
     
     private float INIT_FRAMES = 300;
 
@@ -17,19 +20,67 @@ public class AugmentationController : MonoBehaviour
     public bool scaleAug = false;
     private Vector3 oriScale;
     private Vector3 tarScale;
-    private Vector3 minScale = new Vector3(0.10f, 0.10f, 0.10f);
-    private Vector3 maxScale = new Vector3(0.15f, 0.15f, 0.15f);
+    private Vector3 minScale;
+    private Vector3 maxScale;
+    private Vector3 minScale1 = new Vector3(0.10f, 0.10f, 0.10f);
+    private Vector3 maxScale1 = new Vector3(0.15f, 0.15f, 0.15f);
+    private Vector3 minScale2 = new Vector3(0.4f, 0.2f, 0.001f);
+    private Vector3 maxScale2 = new Vector3(0.6f, 0.3f, 0.001f);
+    private List<string> iconList = new List<string>();
 
     private GameObject curObject;
+
+    private string user = "test.txt";
+    private StreamWriter writer;
+
+    string Vector3ToString(Vector3 v)
+    {
+        string res = v.x + " " + v.y + " " + v.z;
+        return res;
+    }
+
+    string QuaternionToString(Quaternion q)
+    {
+        string res = q.x + " " + q.y + " " + q.z + " " + q.w;
+        return res;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        writer = new StreamWriter(user, false);
         augFrames = INIT_FRAMES;
-        oriScale = minScale;
-        tarScale = maxScale;
         curObject = userInterefaces[Random.Range(0, userInterefaces.Count)];
         print(curObject.transform.name);
+        iconList.Add("Facebook");
+        iconList.Add("Ins");
+        iconList.Add("Tiktok");
+        iconList.Add("Twitter");
+        if (iconList.Contains(curObject.transform.name))
+        {
+            minScale = minScale1;
+            maxScale = maxScale1;
+        }
+        else
+        {
+            minScale = minScale2;
+            maxScale = maxScale2;
+        }
+        oriScale = minScale;
+        tarScale = maxScale;
+        // foreach (GameObject obj in userInterefaces)
+        // {
+        //     float x = Random.Range(-0.5f, 0.5f);
+        //     float y = Random.Range(0.5f, 1.5f);
+        //     float z = Random.Range(0.5f, 1.5f);
+        //     obj.transform.position = new Vector3(x, y, z);
+        // }
+    }
+
+    void OnApplicationQuit()
+    {
+        writer.Flush();
+        writer.Close();
     }
 
     // Update is called once per frame
@@ -38,12 +89,20 @@ public class AugmentationController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             scaleAug = true;
+            writer.WriteLine(curObject.transform.name + " " + Time.time);
+            writer.Flush();
         }
         var eyeTrackingData = TobiiXR.GetEyeTrackingData(TobiiXR_TrackingSpace.World);
         if (scaleAug)
         {
+            string t = "Camera: " + Vector3ToString(camera.transform.position) + " " + QuaternionToString(camera.transform.rotation) + " " + Time.time;
+            writer.WriteLine(t);
+            writer.Flush();
             if(eyeTrackingData.GazeRay.IsValid)
             {   
+                string g = "Gaze: " + Vector3ToString(eyeTrackingData.GazeRay.Origin) + " " + Vector3ToString(eyeTrackingData.GazeRay.Direction) + " " + Time.time;
+                writer.WriteLine(g);
+                writer.Flush();
                 int layerMask = 1 << 6;
                 RaycastHit hit;
                 if (Physics.Raycast(eyeTrackingData.GazeRay.Origin, eyeTrackingData.GazeRay.Direction, out hit, Mathf.Infinity, layerMask))
@@ -65,6 +124,20 @@ public class AugmentationController : MonoBehaviour
                     curFrames = 0;
                     curObject = userInterefaces[Random.Range(0, userInterefaces.Count)];
                     print(curObject.transform.name);
+                    writer.WriteLine("Noticed" + " " + Time.time);
+                    writer.Flush();
+                    if (iconList.Contains(curObject.transform.name))
+                    {
+                        minScale = minScale1;
+                        maxScale = maxScale1;
+                    }
+                    else
+                    {
+                        minScale = minScale2;
+                        maxScale = maxScale2;
+                    }
+                    oriScale = minScale;
+                    tarScale = maxScale;
                 }
             }
 
